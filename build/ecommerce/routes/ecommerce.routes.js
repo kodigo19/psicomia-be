1ae5ad47a7f2b70f4d07c1471e4eb5ddf10171fa
Fernando Segura <fersegura.sa@gmail.com>
2022-06-05 21:05:25 -0500

    srv account

--- build/ecommerce/routes/ecommerce.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const createPreference_controllers_1 = require("../controllers/createPreference.controllers");
-const express_1 = require("express");
-const createPayment_controllers_1 = require("../controllers/createPayment.controllers");
-const router = (0, express_1.Router)();
-router.post('/checkout', createPreference_controllers_1.createPreference);
-router.post('/create_payment', createPayment_controllers_1.createPayment);
-exports.default = router;
--- build/user/entity/models/psychologist.models.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PsychologistModel = void 0;
-const mongoose_1 = __importDefault(require("mongoose"));
-const psychologist_schemas_1 = require("../schemas/psychologist.schemas");
-exports.PsychologistModel = mongoose_1.default.model('Psychologist', psychologist_schemas_1.PsychologistSchema);
